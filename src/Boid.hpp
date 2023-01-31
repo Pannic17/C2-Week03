cddead847bc265d56fdd8bbae58a9726e24c391f
pannic <pannic1984@outlook.com>
2023-01-31 18:08:02 +0000

    Initial Commit

---
+++ src/Boid.hpp
@@ -0,0 +1,69 @@
+//
+//  Boid.hpp
+//  Week03
+//
+//  Created by Nicholas Pan on 2023/1/31.
+//
+
+#ifndef Boid_hpp
+#define Boid_hpp
+
+#include <stdio.h>
+#include "ofMain.h"
+
+class Boid
+{
+// all the methods and variables after the
+// private keyword can only be used inside
+// the class
+private:
+    ofVec3f position;
+    ofVec3f velocity;
+    
+    float separationWeight;
+    float cohesionWeight;
+    float alignmentWeight;
+    
+    float separationThreshold;
+    float neighbourhoodSize;
+    
+    ofVec3f separation(std::vector<Boid *> &otherBoids);
+    ofVec3f cohesion(std::vector<Boid *> &otherBoids);
+    ofVec3f alignment(std::vector<Boid *> &otherBoids);
+    
+// all the methods and variables after the
+// public keyword can only be used by anyone
+public:
+    Boid();
+    Boid(ofVec3f &pos, ofVec3f &vel);
+    
+    ~Boid();
+    
+    ofVec3f getPosition();
+    ofVec3f getVelocity();
+    
+    
+    float getSeparationWeight();
+    float getCohesionWeight();
+    float getAlignmentWeight();
+    
+    float getSeparationThreshold();
+    float getNeighbourhoodSize();
+    
+    void setSeparationWeight(float f);
+    void setCohesionWeight(float f);
+    void setAlignmentWeight(float f);
+    
+    void setSeparationThreshold(float f);
+    void setNeighbourhoodSize(float f);
+    
+    void update(std::vector<Boid *> &otherBoids, ofVec3f &min, ofVec3f &max);
+    
+    void walls(ofVec3f &min, ofVec3f &max);
+    
+    void draw();
+};
+
+
+
+#endif /* Boid_hpp */
---
+++ src/ofApp.cpp
@@ -0,0 +1,91 @@
+#include "ofApp.h"
+
+ofApp::~ofApp(){
+    for (int i = 0; i < boids.size(); i++) {
+        delete boids[i];
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+    int screenW = ofGetScreenWidth();
+    int screenH = ofGetScreenHeight();
+    
+    ofBackground(0, 50, 50);
+    
+    for (int i = 0; i < 50; i++) {
+        boids.push_back(new Boid());
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+    ofVec3f min(0, 0);
+    ofVec3f max(ofGetWidth(), ofGetHeight());
+    
+    for (int i = 0; i < boids.size(); i++) {
+        boids[i]->update(boids, min, max);
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+    for (int i = 0; i < boids.size(); i++) {
+        boids[i]->draw();
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
